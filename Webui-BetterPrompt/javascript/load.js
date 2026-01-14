23131bff8fd8cfc956e3e25514f6bbb72d763f94
realwumbl3 <wumbl3@gmail.com>
2026-01-14 14:20:29 -0500

    Remove WebUI-BetterPrompt

--- Webui-BetterPrompt/javascript/load.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-    try {
-        await import("./src/main.js");
-    } catch (error) {
-        console.error("[Error importing src/main.js]", error);
-    }
-})();
--- Webui-BetterPrompt/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", async () => {
-	try {
-		await import(chrome.runtime.getURL("./javascript/src/main.js"));
-	} catch (error) {
-		console.error("[Error importing src/main.js]", error);
-	}
-});
